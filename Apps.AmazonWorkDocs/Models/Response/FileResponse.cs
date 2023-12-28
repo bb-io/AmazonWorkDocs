898fd2fb181204e8b4561b7f5105d9edacbc685e
bZverok <137277669+bZverok@users.noreply.github.com>
2023-12-28 12:49:40 +0200

    New files (#5)
    * updated metadata
    
    * updated app name
    
    * fixed metadata
    
    * changed logo
    
    * increased version
    
    * added new files
    
    * Update Apps.AmazonWorkDocs.csproj

--- Apps.AmazonWorkDocs/Models/Response/FileResponse.cs
+++ Apps.AmazonWorkDocs/Models/Response/FileResponse.cs
@@ -1,8 +1,8 @@
-using File = Blackbird.Applications.Sdk.Common.Files.File;
+using Blackbird.Applications.Sdk.Common.Files;
 
 namespace Apps.AmazonWorkDocs.Models.Response;
 
 public class FileResponse
 {
-    public File File { get; set; }
+    public FileReference File { get; set; }
 }

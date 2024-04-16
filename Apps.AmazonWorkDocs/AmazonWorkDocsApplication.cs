5e07d61b370f59661b42465902e34723cd059be8
vitalii-bezuhlyi <157373241+vitalii-bezuhlyi@users.noreply.github.com>
2024-04-16 18:50:58 +0300

    Categories (#8)
    * updated metadata
    
    * updated app name
    
    * fixed metadata
    
    * changed logo
    
    * increased version
    
    * fixed download action
    
    * file fixes
    
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * Updated csproj
    
    ---------
    
    Co-authored-by: bzveriok <bohdan.zverok@blackbird.io>
    Co-authored-by: bZverok <137277669+bZverok@users.noreply.github.com>
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>

--- Apps.AmazonWorkDocs/AmazonWorkDocsApplication.cs
+++ Apps.AmazonWorkDocs/AmazonWorkDocsApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.AmazonWorkDocs;
 
-public class AmazonWorkDocsApplication : IApplication
+public class AmazonWorkDocsApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.FileManagementAndStorage];
+        set { }
+    }
+    
     public string Name
     {
         get => "AmazonWorkDocs";
